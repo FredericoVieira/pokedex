980c6cfcc09dab7556b1b1496c80d64a860dc95d
FredericoVieira <fredops1@hotmail.com>
2019-09-01 15:06:53 -0300

    Adds styles for search input.

--- src/ui/search/cmp-search.js
+++ src/ui/search/cmp-search.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Search = ({ onChange, value }) => (
   <input
     type="text"
+    className="search"
     onChange={onChange}
     value={value}
     placeholder="Enter pokemon name..."
