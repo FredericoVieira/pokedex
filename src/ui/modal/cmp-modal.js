bb523619d31d8b681db092714581e94b742913ad
FredericoVieira <fredops1@hotmail.com>
2019-08-31 22:53:50 -0300

    Adds react-lazyload to load sprites on scroll.

--- src/ui/modal/cmp-modal.js
+++ src/ui/modal/cmp-modal.js
@@ -3,11 +3,14 @@ import useGlobal from '../../store'
 
 const Modal = () => {
   const [globalState, globalActions] = useGlobal()
-  const { name, types, height, weight } = globalState.pokemons.selected
+  const { id, name, types, height, weight } = globalState.pokemons.selected
 
   return (
     <div className="modal">
       <section className="modal__main">
+        <img
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+        />
         <span>Name:</span>
         <span>{name}</span>
         <span>Types:</span>
--- src/ui/pokemon/cmp-pokemon.js
+++ src/ui/pokemon/cmp-pokemon.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import useGlobal from '../../store'
+import LazyLoad from 'react-lazyload'
 
 const Pokemon = ({ pokemon }) => {
   const [, globalActions] = useGlobal()
 
   return (
     <div className="pokemon">
-      <button
-        type="button"
-        className="pokemon__sprite"
-        onClick={() => globalActions.getPokemon(pokemon.id)}
-        style={{
-          backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-            pokemon.id
-          }.png`})`
-        }}
-      />
-      <p className="pokemon__name">{pokemon.name}</p>
+      <LazyLoad>
+        <button
+          type="button"
+          className="pokemon__sprite"
+          onClick={() => globalActions.getPokemon(pokemon.id)}
+          style={{
+            backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              pokemon.id
+            }.png`})`
+          }}
+        />
+        <p className="pokemon__name">{pokemon.name}</p>
+      </LazyLoad>
     </div>
   )
 }
