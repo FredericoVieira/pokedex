2ece5b1615a41a62b22932552f2551ac95945520
FredericoVieira <fredops1@hotmail.com>
2019-09-01 21:26:31 -0300

    Updates

---
+++ precache-manifest.a710284c697ad24fcfb51f35da6189f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51e2786b016b154ba837",
+    "url": "/pokedex/static/css/main.1f460e68.chunk.css"
+  },
+  {
+    "revision": "51e2786b016b154ba837",
+    "url": "/pokedex/static/js/main.ec65d233.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "ee8af9f6a10589f9825a",
+    "url": "/pokedex/static/js/2.b563068b.chunk.js"
+  },
+  {
+    "revision": "287c4e50a94adcf9c785164d03d63eaf",
+    "url": "/pokedex/index.html"
+  }
+];
